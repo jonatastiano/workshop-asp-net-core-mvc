c503ea2da2ea4f3fa9c964d2178093a02a649c20
Jonatas Tiano <jonatasepriscilatiano123@gmail.com>
2020-02-08 19:45:05 -0300

    VendedorFormViewModel e componente select para departamento

--- SalesWebMvc/Controllers/VendedoresController.cs
+++ SalesWebMvc/Controllers/VendedoresController.cs
@@ -5,16 +5,19 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Servicos;
 using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 
 namespace SalesWebMvc.Controllers
 {
     public class VendedoresController : Controller
     {
         private readonly VendedorServico _vendedorServico;
+        private readonly DepartamentoServico _departamentoServico;
 
-        public VendedoresController(VendedorServico vendedorServico)
+        public VendedoresController(VendedorServico vendedorServico, DepartamentoServico departamentoServico)
         {
             _vendedorServico = vendedorServico;
+            _departamentoServico = departamentoServico;
         }
 
         public IActionResult Index()
@@ -25,7 +28,9 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult Criar()
         {
-            return View();
+            var departamentos = _departamentoServico.FindAll();
+            var viewModel = new VendedorFormViewModel { Departamentos = departamentos };
+            return View(viewModel);
         }
 
         [HttpPost]
---
+++ SalesWebMvc/Servicos/DepartamentoServico.cs
@@ -0,0 +1,22 @@
+﻿using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Servicos
+{
+    public class DepartamentoServico
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public DepartamentoServico(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Departamento> FindAll()
+        {
+            return _context.Departamento.OrderBy(x => x.Nome).ToList();
+        }
+    }
+}
